2d1be2e05a5619d978a107d3c4d1992d5aa77ede
Giovanna <mennezes.giovanna@gmail.com>
2026-08-03 21:20:58 -0300

    fix: get e set faltantes

--- src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -81,4 +81,10 @@ public class Postagem {
 	public void setData(LocalDateTime data) {
 		this.data = data;
 	}
+	public Usuario getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
 }
